3bdf3652d1e62a45933002c227a4361abe6ff266
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-04 14:52:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyRemoveFirstDriver() {
 function appendDriver(name) {
   let arr = drivers.slice();
   arr.push(name);
-  // return arr;
+  return arr;
 }
 
 function prependDriver(name) {
