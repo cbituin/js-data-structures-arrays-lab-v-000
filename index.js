c2f96d20407ab0694730c55b9b6a8505a6276ad9
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-04 14:47:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 let drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
-  drivers.push(name)
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
 }
