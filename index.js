1dda67f31a51112e8aa099bab443cc96fdf2f329
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-04 14:45:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your solution here!
 let drivers = ["Milo", "Otis", "Garfield"];
 
-function removeFirstDriver(name) {
-  let removedFirst = name.slice(0);
-  return removedFirst;
+function destructivelyAppendDriver(name) {
+  name.slice()  
 }
